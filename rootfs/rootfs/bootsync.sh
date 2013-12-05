b9bc6539a983f549bd1e4c626a256bcc19df9217
Tianon Gravi <admwiggin@gmail.com>
2013-12-04 17:41:51 -0700

    Add cool "boot2docker" hostname and the contents of the original upstream bootsync script

--- rootfs/rootfs/bootsync.sh
+++ rootfs/rootfs/bootsync.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+/usr/bin/sethostname boot2docker
+
 if grep -q '^docker:' /etc/passwd; then
   # if we have the docker user, let's create the docker group
   /bin/addgroup -S docker
   # ... and add our docker user to it!
   /bin/addgroup docker docker
 fi
+
+/opt/bootlocal.sh &
