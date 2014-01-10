51b652f8958c0cdacb399d6e3e02334874d196e2
Steeve Morin <steeve.morin@gmail.com>
2014-01-10 15:18:41 +0100

    Indent changes.

--- rootfs/rootfs/bootsync.sh
+++ rootfs/rootfs/bootsync.sh
@@ -3,10 +3,10 @@
 /usr/bin/sethostname boot2docker
 
 if grep -q '^docker:' /etc/passwd; then
-  # if we have the docker user, let's create the docker group
-  /bin/addgroup -S docker
-  # ... and add our docker user to it!
-  /bin/addgroup docker docker
+    # if we have the docker user, let's create the docker group
+    /bin/addgroup -S docker
+    # ... and add our docker user to it!
+    /bin/addgroup docker docker
 fi
 
 # Load TCE extensions
