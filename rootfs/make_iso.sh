99c77d9eff1e94db4770bd3da32223fc4ead73e2
Steeve Morin <steeve.morin@gmail.com>
2014-02-19 01:37:47 +0100

    Make Kernel version and AUFS branches more easily configurable.

--- rootfs/make_iso.sh
+++ rootfs/make_iso.sh
@@ -26,7 +26,7 @@ chmod +x $ROOTFS/opt/shutdown.sh
 
 # Prepare the ISO directory with the kernel
 mkdir -p /tmp/iso/boot
-cp -v /linux-3.12.1/arch/x86_64/boot/bzImage /tmp/iso/boot/vmlinuz64
+cp -v /linux-kernel/arch/x86_64/boot/bzImage /tmp/iso/boot/vmlinuz64
 cp -vr /isolinux /tmp/iso/boot
 
 # Pack the rootfs
