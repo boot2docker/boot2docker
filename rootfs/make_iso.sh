763d3c5749b4e83961b50cd82968728f413472e1
Tianon Gravi <admwiggin@gmail.com>
2016-09-28 10:12:23 -0700

    Shrink the ISO label ("xorriso : FAILURE : -volid: Text too long (36 > 32)")

--- rootfs/make_iso.sh
+++ rootfs/make_iso.sh
@@ -44,7 +44,7 @@ cd -
 xorriso  \
     -publisher "Docker Inc." \
     -as mkisofs \
-    -l -J -R -V "Boot2Docker-v$(cat $ROOTFS/etc/version)" \
+    -l -J -R -V "b2d-v$(cat $ROOTFS/etc/version)" \
     -no-emul-boot -boot-load-size 4 -boot-info-table \
     -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat \
     -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
